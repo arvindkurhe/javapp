d96351e11b89ad03a96b73da1d7c9b1d99b6c103
arvindkurhe <arvindk@cybage.com>
2023-02-03 13:59:21 +0530

    Update Sample2.java

--- test-coverage-report-jacoco/module2/src/main/java/com/coveragedemo/module2/Sample2.java
+++ test-coverage-report-jacoco/module2/src/main/java/com/coveragedemo/module2/Sample2.java
@@ -2,6 +2,7 @@ package com.coveragedemo.module2;
 
 public class Sample2 {
     public  int getValue() {
+        GoSellSDK.init(this, "sk_test_kovrMB0mupFJXfNZWx6Etg5y","company.tap.goSellSDKExample");  // to be replaced by merchant
         return 10;
     }
 
